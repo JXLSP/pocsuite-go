7f2126e0a31cbbc70ddc7d0f2cc8b7ef171a0048
seaung <seaung.c0m7de@protonmail.com>
2024-05-10 21:51:12 +0800

    shellcode 基类实现

---
+++ internal/pocsuite3/shellcode/shellcode.go
@@ -0,0 +1,46 @@
+package shellcode
+
+import (
+	"strconv"
+	"strings"
+)
+
+type ShellCoder interface {
+    GenShellCode(code string) string
+    MakeInline(payloads string) string
+    GetShellCode() string
+}
+
+type ShellCodeBase struct {
+    Target      string
+    Arch        string
+    IP          string
+    Port        int
+    Name        string
+    Prefix      string
+    Suffix      string
+    BadChars    string
+}
+
+func NewShellCodeBase() *ShellCodeBase {
+    return &ShellCodeBase{}
+}
+
+func (s *ShellCodeBase) GenShellCode(code string) string {
+    if code != "" {
+        code = strings.ReplaceAll(code, "{{LOCALHOST}}", s.IP)
+        code = strings.ReplaceAll(code, "{{LOCALPORT}}", strconv.Itoa(s.Port))
+    }
+    return code
+}
+
+func (s *ShellCodeBase) MakeInline(payloads string) string {
+    payloads = strings.ReplaceAll(payloads, "\t", " ")
+    payloads = strings.ReplaceAll(payloads, "\r", " ")
+    payloads = strings.ReplaceAll(payloads, "\n", " ")
+    return payloads
+}
+
+func (s *ShellCodeBase) GetShellCode() string {
+    return ""
+}
